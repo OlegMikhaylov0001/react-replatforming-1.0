6405fec336e79b1a347a827e08d2eb299ca03736
OlegMikhaylovE <103054177+OlegMikhaylov0001@users.noreply.github.com>
2023-01-23 12:10:38 +0400

    origin/ENFD-403/add_eslint_ptettier

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,11 @@ import { setupStore } from './store';
 
 const store = setupStore();
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
